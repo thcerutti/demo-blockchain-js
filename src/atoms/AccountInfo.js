4944e81d12a746a55550a3c994cc3f69800d9e65
Thiago Cerutti <thiagocerutti@gmail.com>
2022-10-12 17:49:03 -0300

    chore: refactor components + clean up

--- src/atoms/AccountInfo.js
+++ src/atoms/AccountInfo.js
@@ -1,6 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 import Card from "react-bootstrap/Card";
+const Web3 = require("web3");
 
-export const AccountInfo = ({ accountId }) => (
-  <Card body>Connected Account: {accountId}</Card>
-);
+export const AccountInfo = ({ accountId }) => {
+  const [balance, setBalance] = useState('');
+  const web3 = new Web3(Web3.givenProvider);
+
+  const getAccountBalance = (accountId) => {
+    web3.eth.getBalance(accountId, (_, balance) => {
+      setBalance(balance.toString());
+    });
+  };
+  getAccountBalance(accountId);
+  return (
+    <Card body>
+      <p>
+        <strong>Connected Account: </strong> {accountId}
+      </p>
+      <p>
+        <strong>Balance: </strong>
+        {web3.utils.fromWei(balance, 'ether')} ETH
+      </p>
+    </Card>
+  );
+};
--- src/molecules/MetamaskConnection.js
+++ src/molecules/MetamaskConnection.js
@@ -6,6 +6,7 @@ import { AccountInfo } from "../atoms/AccountInfo";
 export const ConnectToMetamask = () => {
   const [currentAccount, setCurrentAccount] = useState([]);
   const [isConnected, setIsConnected] = useState(false);
+  const [isMetamaskPresent, setIsMetamaskPresent] = useState(false);
 
   const connect = async () => {
     window.ethereum
@@ -14,15 +15,14 @@ export const ConnectToMetamask = () => {
       .catch(console.log);
   };
 
-  detectEthereumProvider().then((provider) => {
+  detectEthereumProvider({ silent: true }).then((provider) => {
     if (provider && provider.isMetaMask) {
       provider.on("accountsChanged", setCurrentAccount);
-    } else {
-      console.log("Please install MetaMask!");
+      setIsMetamaskPresent(true);
     }
   });
 
-  window.ethereum.request({ method: "eth_accounts" }).then((accounts) => {
+  window.ethereum?.request({ method: "eth_accounts" }).then((accounts) => {
     setIsConnected(accounts.length > 0);
     if (isConnected) {
       setCurrentAccount(accounts[0]);
@@ -31,11 +31,13 @@ export const ConnectToMetamask = () => {
 
   return (
     <>
-      {!isConnected ? (
+      {!isMetamaskPresent ? (
+        <span>no metamask installed</span>
+      ) : isMetamaskPresent && !isConnected ? (
         <Button onClick={() => connect()}>Connect to metamask</Button>
       ) : (
         <div>
-          <AccountInfo accountId={currentAccount}/>
+          <AccountInfo accountId={window.ethereum?.selectedAddress} />
         </div>
       )}
     </>
